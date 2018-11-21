770f6db6ab7bd16eb1e7c5b9985b9b0dcd1d64fa
Matt Cooke <cookem529@gmail.com>
2018-11-21 13:30:31 -0500

    changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,3 @@
 class Event < ApplicationRecord
-  # validates :name, presence: true
-  # validates :due_date_or_recurring_date, presence: true
-  # validates :amount_due, presence: true
-  # validates :flow, presence: true
+
 end
