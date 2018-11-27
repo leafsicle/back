d824f332f1a27e739bf64154ef053d976c6303aa
Matt Cooke <cookem529@gmail.com>
2018-11-27 17:45:06 -0500

    re attempting push to heroku master after fixing error in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
   def fallback_index.html
     if Rails.env.production?
-      render :file => "public.index.html", layout: false
-    # else
-    #   redirect_to "admin/events"
+      render :file => "public/index.html", layout: false
     end
+  end
 end
