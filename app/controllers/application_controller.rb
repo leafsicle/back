4ea6dadd816effb3bc0f74c088850a9231de1ec6
Matt Cooke <cookem529@gmail.com>
2018-11-27 17:17:10 -0500

    changed routes and application contr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+  def fallback_index.html
+    if Rails.env.production?
+      render :file => "public.index.html", layout: false
+    # else
+    #   redirect_to "admin/events"
+    end
 end
