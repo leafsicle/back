49d59def14334a99dd70387b614d0a5e734f3376
Matt Cooke <cookem529@gmail.com>
2018-11-28 13:26:03 -0500

    rename action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
-  def fallback_index.html
+  def fallback_index_html
     if Rails.env.production?
       render :file => "public/index.html", layout: false
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "*path", to:"application#fallback_index.html"
-
-
-
+  get "*path", to: "application#fallback_index_html"
 end
