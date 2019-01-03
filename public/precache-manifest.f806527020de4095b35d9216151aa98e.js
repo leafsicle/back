7adad74e271c2c3c5759dac8eb83243b6b2ff345
Matthew Cooke <cookem529@gmail.com>
2019-01-03 15:39:59 -0500

    Adding things

---
+++ public/precache-manifest.f806527020de4095b35d9216151aa98e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e83e80e3b0076daa49de",
+    "url": "/static/css/main.1a8c9bda.chunk.css"
+  },
+  {
+    "revision": "e83e80e3b0076daa49de",
+    "url": "/static/js/main.e83e80e3.chunk.js"
+  },
+  {
+    "revision": "bbe69c20f43e65cf6c34",
+    "url": "/static/js/1.bbe69c20.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24b710888f6c3120a741b5d2fb77efa3",
+    "url": "/index.html"
+  }
+];
